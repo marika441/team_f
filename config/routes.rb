e603254c887566d699c6746b75027466eea38322
EC2 Default User <s.marika441@gmail.com>
2021-12-12 14:34:43 +0000

    [Add]topページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :customer do
+    root 'homes#top'
+  end
+
 end
